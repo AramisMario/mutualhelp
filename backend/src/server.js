f1b08c9bfa3d068d1fa2f1ae2193487f1c431432
AramisMario <aramismario@gmail.com>
2020-03-16 19:44:09 -0500

    servidor basado en clases
    se creo la clase Server dentro del archivo server

---
+++ backend/src/server.js
@@ -0,0 +1,50 @@
+import express from "express";
+import path from 'path';
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import userRouter from "./routes/userRouter";
+import authRouter from "./routes/authRouter";
+
+class Server{
+    constructor(){
+        dotenv.config();
+        this.app = express();
+        this.conection();
+        this.config();
+        this.routes();
+
+    }
+
+    async conection(){
+        const MONGO_URI = `mongodb://${process.env.DB_USER}:${process.env.DBPASSWORD}@localhost/mutualhelp`;
+        try{
+            await mongoose.connect(MONGO_URI,{
+                useCreateIndex:true,
+                useNewUrlParser:true,
+                useFindAndModify:true,
+                useUnifiedTopology:true,
+                });
+                console.log("db is conected");
+        }catch(Error){
+            console.log(Error);
+        }
+    }
+
+    config(){
+        this.app.use(express.json());
+    }
+    routes(){
+        this.app.use('/api/user',userRouter);
+        this.app.use('/api/auth',authRouter);
+    }
+    start(){
+        this.app.listen(7000, ()=>console.log("app runing on port 7000"));
+    }
+}
+
+const server = new Server();
+export default server;
+
+
+
+
